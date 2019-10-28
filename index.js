a799216666cc9898969def7386ca64e8a01f37cb
John Karrell Speck <speckworks@gmail.com>
2019-10-28 01:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
  prepend.unshift("Odie");
 const removeFirst = ["Milo", "Otis", "Garfield"];
-  removeFirst.shift;
+  removeFirst.shift();
 const removeLast = ["Milo", "Otis", "Garfield"]
-  removeLast.pop;
+  removeLast.pop();
