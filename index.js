8b6f57579185693e5ecd1aab44b98bc01f81371e
John Karrell Speck <speckworks@gmail.com>
2019-10-28 01:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
   append.push("Odie");
-const = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
  prepend.unshift("Odie");
 const removeFirst = ["Milo", "Otis", "Garfield"];
   removeFirst.shift;
