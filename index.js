8d221d70170080a7a416052b08edee7104f839e2
John Karrell Speck <speckworks@gmail.com>
2019-10-28 01:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Write your solution here!
-const append.shift("Odie") = ["Milo", "Otis", "Garfield"]
-  
-const prepend.unshift("Odie") = ["Milo", "Otis", "Garfield"]
-const removeFirst.shift = ["Milo", "Otis", "Garfield"]
-const removeLast.pop = ["Milo", "Otis", "Garfield"]
+const append = ["Milo", "Otis", "Garfield"];
+  append.push("Odie");
+const = ["Milo", "Otis", "Garfield"];
+ prepend.unshift("Odie");
+const removeFirst = ["Milo", "Otis", "Garfield"];
+  removeFirst.shift;
+const removeLast = ["Milo", "Otis", "Garfield"]
+  removeLast.pop;
